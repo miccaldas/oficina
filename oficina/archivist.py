414ade294397132b5e21096390f7ed0a5f8921aa
mclds <mclds@protonmail.com>
2023-02-12 00:06:28 +0000

    12-02-2023 commit

--- oficina/archivist.py
+++ oficina/archivist.py
@@ -1,9 +1,12 @@
 """Module Docstring"""
+import os
+import subprocess
+
 import snoop
 from ranger import PY3
 from ranger.api.commands import Command
 from snoop import pp
-import os
+
 
 def type_watch(source, value):
     return "type({})".format(source), type(value)
@@ -12,11 +15,14 @@ def type_watch(source, value):
 snoop.install(watch_extras=[type_watch])
 
 
-class archivist(Command):
-    """"""
-
+class archive(Command):
+    @snoop
     def execute(self):
         """"""
 
+        path = f"{self.fm.thisdir}/{self.fm.thisfile}"
+        cmd = f"atool -x {path}"
+        subprocess.run(cmd, shell=True)
 
-
+    if __name__ == "__main__":
+        execute()
